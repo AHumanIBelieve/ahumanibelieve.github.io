19d798ba0c54a1c54224d70de591d0df4538f739
AHumanIBelieve <chaityajain@outlook.com>
2023-01-09 18:04:06 +0000

    js added

---
+++ js/writeStuff.js
@@ -0,0 +1,18 @@
+
+ document.querySelector(".writeHeader").innerHTML = `
+    <div>
+        <img src="images/logo.png" alt="Wizards and Potters Logo">
+        <title>Wizards & Potters, a Harry Potter/DnD webcomic</title>
+        <div class="header">
+            <h3 id="nav"><a id="headernav" href="0005.html">READ</a> ¦ ¦ ¦ ¦ <a id="headernav" href="archive.html">ARCHIVE</a> ¦ ¦ ¦ ¦ <a id="headernav" href="about.html">ABOUT</a></h3>
+        </div>
+    </div>
+`;
+
+document.querySelector(".writeFooter").innerHTML = `
+<div>
+    <br>
+    <br>
+    <p id="footer">ⒸAHumanIBelieve, 2023</p>
+</div>
+`;
