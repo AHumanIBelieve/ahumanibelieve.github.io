f97c0dd63e51f5172665dc65a020d4430555759d
AHumanIBelieve <chaityajain@outlook.com>
2023-01-16 19:58:53 +0000

    Update writeStuff.js

--- js/writeStuff.js
+++ js/writeStuff.js
@@ -4,7 +4,7 @@
         <img src="images/logo.png" alt="Wizards and Potters Logo">
         <title>Wizards & Potters, a Harry Potter/DnD webcomic</title>
         <div class="header">
-            <h3 id="nav"><a id="headernav" href="0007.html">READ</a> ¦ ¦ ¦ ¦ <a id="headernav" href="archive.html">ARCHIVE</a> ¦ ¦ ¦ ¦ <a id="headernav" href="about.html">ABOUT</a></h3>
+            <h3 id="nav"><a id="headernav" href="0008.html">READ</a> ¦ ¦ ¦ ¦ <a id="headernav" href="archive.html">ARCHIVE</a> ¦ ¦ ¦ ¦ <a id="headernav" href="about.html">ABOUT</a></h3>
         </div>
     </div>
 `;
